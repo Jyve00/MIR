8386e688eac9b00a9319f6f81b7099d4f99f3bcf
Stephen William <swilliam.productions@gmail.com>
2021-11-16 15:49:57 -0500

    cleaning

--- functions.py
+++ functions.py
@@ -11,13 +11,13 @@ import matplotlib.pyplot as plt
 sr = 22050     # sample rate. based on Nyquist frequency, we only care about frequencies up to 10kHz therefor the sample rate will only perserve those frequencies 
 n_fft = 2048
 hop_length = 512
-duration = 30 # length of song to be used (in seconds) 
+duration = 5 # length of song to be used (in seconds) 
 
 # the 4 functions are based off the 4 modules mentioned in Librosa Paper
 
 
 # function to perform all required mathmatical transforms 
-def transform(filename, sample_rate=22050):
+def get_mels(filename, sample_rate=22050):
     y, sr = librosa.load(
         path = filename,  # load in audio file. MP3 not supported refer to Librosa documentation 
         sr = sample_rate, # by convention the default sample rate is 22050, lower if not enough processing power 
@@ -27,57 +27,57 @@ def transform(filename, sample_rate=22050):
         res_type = 'kaiser_best'
         )
 
-    D = librosa.stft(y)         #Short-Time Fast Fourier Transform
-    S = np.abs(librosa.stft(y)) # Spectrogram 
-    C = librosa.cqt(y, sr)      # CQT 
+    
 
-    return y, sr, D, S, C 
+    return y, sr, 
 
-
+#D = librosa.stft(y)         #Short-Time Fast Fourier Transform
+    #S = np.abs(librosa.stft(y)) # Spectrogram 
+    #C = librosa.cqt(y, sr)      # CQT 
 
 # features extraction 
 
-def features(y, sr)
+#def features(y, sr)
 
-    M = librosa.feature.melspectrogram(y=y, sr=sr)
-    MFCC = librosa.feature.mfcc(y=y, sr=sr)
-    chroma = librosa.feature.chroma_cqt(y=y, sr=sr)
-    tonnetz = librosa.feature.tonnetz(y=y, sr=sr) # Computes the tonal centroid features 
+    #M = librosa.feature.melspectrogram(y=y, sr=sr)
+    #MFCC = librosa.feature.mfcc(y=y, sr=sr)
+    #chroma = librosa.feature.chroma_cqt(y=y, sr=sr)
+    #tonnetz = librosa.feature.tonnetz(y=y, sr=sr) # Computes the tonal centroid features 
     #delta = librosa.feature.delta()
 
 
-    return M, MFCC, chroma, tonnetz, 
+    #return M, MFCC, chroma, tonnetz, 
 
 
-def efx(y, sr):
+#def efx(y, sr):
     y_harmonic, y_percussive = librosa.effects.hpss(y)  
 
 
 
-def beats(y , sr):
-    onset_envelope = librosa.onset.onset_strength(y, sr)
-    onsets = librosa.onset.onset_detect(onset_envelope=onset_envelope)
+#def beats(y , sr):
+    #onset_envelope = librosa.onset.onset_strength(y, sr)
+    #onsets = librosa.onset.onset_detect(onset_envelope=onset_envelope)
 
-    return onsets,  
+    #return onsets,  
 
 
 # visuals 
-def visuals():
+#def visuals():
     # display spectrogram
-    log_power = librosa.logamplitude(C**2, ref_power = np.max, top_db=40)
-    log_specshow = librosa.display.specshow(log_power, x_axis='time', y_axis='log')
-    plt.colorbar()
+    #log_power = librosa.logamplitude(C**2, ref_power = np.max, top_db=40)
+    #log_specshow = librosa.display.specshow(log_power, x_axis='time', y_axis='log')
+    #plt.colorbar()
 
 
-    cqt_plot = librosa.display.specshow(chroma, x_axis='time', y_axis='chroma')
-    return log_specshow, cqt_plot
+    #cqt_plot = librosa.display.specshow(chroma, x_axis='time', y_axis='chroma')
+    #return log_specshow, cqt_plot
 
-    plt.subplot(2,1 ,1)
-    plt.plot(onset_envelope, label = 'Onset strength')
-    plt.vlines(onset, 0, onset_envelope.max(), color='r', alpha=0.25)
-    plt.xticks([]), plt.yticks([])
-    plt.legend(frameon=True)
-    plt.axis('tight')
+    #plt.subplot(2,1 ,1)
+    #plt.plot(onset_envelope, label = 'Onset strength')
+    #plt.vlines(onset, 0, onset_envelope.max(), color='r', alpha=0.25)
+    #plt.xticks([]), plt.yticks([])
+    #plt.legend(frameon=True)
+    #plt.axis('tight')
 
 
 
